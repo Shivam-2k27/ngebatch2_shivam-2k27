748e31ac8cb63116aee60707cc548ad095786db7
Shivam <shivam.chaudhary@accenture.com>
2024-06-10 07:47:08 +0000

    week 2 content

---
+++ Week2/DAY1/Deepcopy.ts
@@ -0,0 +1,15 @@
+type user={
+    name:string,
+    age:number
+}
+
+const users={
+    shaw:{
+        surname:'shaw'
+    },
+    shivam:{
+        surname:'shivam'
+    }
+}
+
+
---
+++ Week2/DAY1/HOF.ts
@@ -0,0 +1,15 @@
+const userDAta=[
+    {
+        name:'shivam',
+        age:24
+    },
+    {
+        name:'chaudhary',
+        age:23
+    },
+]
+
+const newarray =userDAta.filter((item)=> item.name==='shivam');
+
+
+ console.log(newarray); 
